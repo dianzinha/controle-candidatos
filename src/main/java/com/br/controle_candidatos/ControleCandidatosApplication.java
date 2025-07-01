b7f016fff865d88a98cbc374e1064fd59330662f
Caroline Gardeli Dian <carolinegardeli@hotmail.com>
2025-07-01 18:24:11 -0300

    Criando a base da main com os parametros iniciais de analise de candidatura

--- src/main/java/com/br/controle_candidatos/ControleCandidatosApplication.java
+++ src/main/java/com/br/controle_candidatos/ControleCandidatosApplication.java
@@ -5,9 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ControleCandidatosApplication {
+	public static void main(String[] args){
 
-	public static void main(String[] args) {
-		SpringApplication.run(ControleCandidatosApplication.class, args);
+		System.out.println("Controle de Candidatos - DIO");
+		analise(1900.00);
+		analise(2200.00);
+		analise(2000.00);
+	}
+
+	static void analise(double salarioPretendido) {
+		double salarioBase = 2000.00;
+
+		if(salarioBase > salarioPretendido) {
+			System.out.println("LIGAR PARA O CANDIDATO");
+		} else if(salarioBase == salarioPretendido)
+			System.out.println("LIGAR PARA O CANDIDATO COM CONTRA-PROPOSTA");
+		else {
+			System.out.println("AGUARDANDO OS DEMAIS CANDIDATOS");
+		}
 	}
 
 }
