e8166b0da69461642cf5b655b520f30ff29d0951
Caroline Gardeli Dian <carolinegardeli@hotmail.com>
2025-07-01 18:11:37 -0300

    Criando a arquiterura do projeto

---
+++ src/test/java/com/br/controle_candidatos/ControleCandidatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.controle_candidatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleCandidatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
